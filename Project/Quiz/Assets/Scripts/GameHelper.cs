346f49b87dfe80ff1477044ffcef489a2a60ace0
AlekseiDP <46382495+AlekseiDP@users.noreply.github.com>
2019-01-27 13:57:25 +0300

    Движение экрана вверх

---
+++ Project/Quiz/Assets/Scripts/GameHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHelper : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SlideUp()
+    {
+        GetComponent<Animation>().Play("ScreenSlideUp");
+    }
+}
